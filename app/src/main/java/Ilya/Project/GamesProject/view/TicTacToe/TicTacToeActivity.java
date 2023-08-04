5a1eed171addb9b40363a585a03ded1f039037d1
ilya1200 <ilya1200@gmail.com>
2023-08-04 12:58:46 +0300

    Refactor TicTacToeActivity and model

--- app/src/main/java/Ilya/Project/GamesProject/view/TicTacToe/TicTacToeActivity.java
+++ app/src/main/java/Ilya/Project/GamesProject/view/TicTacToe/TicTacToeActivity.java
@@ -2,6 +2,7 @@ package Ilya.Project.GamesProject.view.TicTacToe;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -18,7 +19,6 @@ import java.util.List;
 import java.util.UUID;
 
 import Ilya.Project.GamesProject.R;
-import Ilya.Project.GamesProject.model.data.game.Player;
 import Ilya.Project.GamesProject.utils.Constants;
 import Ilya.Project.GamesProject.view.gameList.GameListActivity;
 
@@ -27,44 +27,44 @@ public class TicTacToeActivity extends AppCompatActivity implements View.OnClick
     TicTacToeViewModel ticTacToeViewModel;
     private MaterialTextView firstUserTextView;
     private MaterialTextView secondUserTextView;
-    private MaterialButton quitGameBtn;
     private UUID gameId;
 
     @Override
     protected void onPause() {
-        super.onPause();
+        Log.d("Game", "onPause");
         ticTacToeViewModel.stopPollingGameUpdates();
+        super.onPause();
     }
 
     @Override
     protected void onResume() {
-        super.onResume();
+        Log.d("Game", "onResume");
         ticTacToeViewModel.startPollingGameUpdates(gameId);
+        super.onResume();
     }
 
     @Override
     protected void onDestroy() {
-        super.onDestroy();
+        Log.d("Game", "onDestroy");
         ticTacToeViewModel.stopPollingGameUpdates();
+        super.onDestroy();
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("Game", "onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tic_tac_toe);
+        ticTacToeViewModel = new ViewModelProvider(this).get(TicTacToeViewModel.class);
 
-        if (!getIntent().getExtras().containsKey(Constants.GAME_ID)) {
+        if (getIntent().getExtras() == null || !getIntent().getExtras().containsKey(Constants.GAME_ID)) {
             Toast.makeText(TicTacToeActivity.this, R.string.internal_error, Toast.LENGTH_LONG).show();
             finish();
+            return;
         }
-
-        ticTacToeViewModel = new ViewModelProvider(this).get(TicTacToeViewModel.class);
-        initObservers();
-
         gameId = UUID.fromString(getIntent().getStringExtra(Constants.GAME_ID));
         initLayout();
-
-        quitGameBtn.setOnClickListener(v -> showQuitConfirmationDialog());
+        initObservers();
     }
 
     @Override
@@ -77,44 +77,15 @@ public class TicTacToeActivity extends AppCompatActivity implements View.OnClick
         ticTacToeViewModel.gameUpdates.observe(this, game -> {
             firstUserTextView.setText(game.getUserFirstName());
             secondUserTextView.setText(game.getUserSecondName());
-            
+            ticTacToeViewModel.updateBoard(game, buttons);
         });
-        ticTacToeViewModel.leaveGameSuccess.observe(this, leaveGameSuccess -> {
-            moveToActivity(new Intent(TicTacToeActivity.this, GameListActivity.class));
-        });
-        ticTacToeViewModel.enabledCells.observe(this, shouldEnable ->{
-            int numRows = shouldEnable.length;
-            int numCols = shouldEnable[0].length;
-
-            for (int row = 0; row < numRows; row++) {
-                for (int col = 0; col < numCols; col++) {
-                    int pos = row * numCols + col;
-                    if (pos < buttons.size()) {
-                        buttons.get(pos).setEnabled(shouldEnable[row][col]);
-                    }
-                }
-            }
-        });
-
-        ticTacToeViewModel.playerCells.observe(this, playerCells ->{
-            int numRows = playerCells.length;
-            int numCols = playerCells[0].length;
-
-            for (int row = 0; row < numRows; row++) {
-                for (int col = 0; col < numCols; col++) {
-                    int pos = row * numCols + col;
-                    Player player = playerCells[row][col];
-                    if (pos < buttons.size() && player != null) {
-                        buttons.get(pos).setBackgroundResource(player == Player.FIRST? R.drawable.tictactoe_x:R.drawable.tictactoe_o);
-                    }
-                }
-            }
-        });
+        ticTacToeViewModel.leaveGameSuccess.observe(this, leaveGameSuccess -> moveToActivity(new Intent(TicTacToeActivity.this, GameListActivity.class)));
         ticTacToeViewModel.endGameDialogMessage.observe(this, this::showEndGameDialog);
     }
 
     private void initLayout() {
-        quitGameBtn = findViewById(R.id.quit_game_btn);
+        MaterialButton quitGameBtn = findViewById(R.id.quit_game_btn);
+        quitGameBtn.setOnClickListener(v -> showQuitConfirmationDialog());
         firstUserTextView = findViewById(R.id.first_user);
         secondUserTextView = findViewById(R.id.second_user);
 
@@ -140,20 +111,14 @@ public class TicTacToeActivity extends AppCompatActivity implements View.OnClick
 
         for (Button b : buttons) {
             b.setOnClickListener(this);
+            b.setEnabled(false);
         }
-        setButtonsEnable(false);
     }
 
     @Override
     public void onClick(View v) {
         String move = (String) v.getTag();
         ticTacToeViewModel.handleMakeMoveUpdates(gameId, move);
-    }
-
-    public void setButtonsEnable(boolean isEnable) {
-        for (Button b : buttons) {
-            b.setEnabled(isEnable);
-        }
     }
 
     private void moveToActivity(Intent intent) {
@@ -171,15 +136,14 @@ public class TicTacToeActivity extends AppCompatActivity implements View.OnClick
         alertDialog.show();
     }
 
-
     private void showQuitConfirmationDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setMessage(getString(R.string.quit_dialog_message))
                 .setTitle(getString(R.string.quit_dialog_title))
                 .setPositiveButton(getString(R.string.quit_dialog_positive_button), (dialog, id) -> ticTacToeViewModel.leaveGame(gameId))
-                .setNegativeButton(getString(R.string.quit_dialog_negative_button), (dialog, id) -> {});
+                .setNegativeButton(getString(R.string.quit_dialog_negative_button), (dialog, id) -> {
+                });
         AlertDialog dialog = builder.create();
         dialog.show();
     }
-
 }
--- app/src/main/java/Ilya/Project/GamesProject/view/TicTacToe/TicTacToeViewModel.java
+++ app/src/main/java/Ilya/Project/GamesProject/view/TicTacToe/TicTacToeViewModel.java
@@ -1,10 +1,13 @@
 package Ilya.Project.GamesProject.view.TicTacToe;
 
 import android.content.Context;
+import android.os.Handler;
+import android.widget.Button;
 
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -20,34 +23,27 @@ import Ilya.Project.GamesProject.utils.DataResult;
 import Ilya.Project.GamesProject.utils.providers.ContextProvider;
 
 public class TicTacToeViewModel extends ViewModel {
+    public static AtomicBoolean keepUpdating = new AtomicBoolean(false);
     public MutableLiveData<Game> gameUpdates = new MutableLiveData<>();
-
-    public MutableLiveData<boolean[][]> enabledCells = new MutableLiveData<>();
-
-    public MutableLiveData<Player[][]> playerCells = new MutableLiveData<>();
-
     public MutableLiveData<String> showErrorMessageToastLiveData = new MutableLiveData<>();
-
     public MutableLiveData<Boolean> leaveGameSuccess = new MutableLiveData<>();
     public MutableLiveData<String> endGameDialogMessage = new MutableLiveData<>();
-    public static AtomicBoolean keepUpdating = new AtomicBoolean(false);
-
-    private final boolean[][] _enabledCells = new boolean[3][3];
-
     private boolean isMyTurn = false;
+    private final Handler handler = new Handler();
+
 
 
     public void handleMakeMoveUpdates(UUID gameId, String move) {
         User user = UserRepository.getUser();
         Game game = gameUpdates.getValue();
-        if (user==null || game == null || game.getGameStatus() != GameStatus.PLAYING){
+        if (user == null || game == null || game.getGameStatus() != GameStatus.PLAYING) {
             return;
         }
 
         Player player = game.getCurrentPlayer();
         String username = user.getUsername();
-        isMyTurn = player==Player.FIRST && game.getUserFirstName().equals(username) || player==Player.SECOND && game.getUserSecondName().equals(username);
-        if(!isMyTurn) {
+        isMyTurn = player == Player.FIRST && game.getUserFirstName().equals(username) || player == Player.SECOND && game.getUserSecondName().equals(username);
+        if (!isMyTurn) {
             return;
         }
 
@@ -83,46 +79,25 @@ public class TicTacToeViewModel extends ViewModel {
         });
     }
 
-    public void processGameUpdate(Game game){
-        User user = UserRepository.getUser();
-        if (user == null){
-            return;
-        }
-        Player player = game.getCurrentPlayer();
-        String username = user.getUsername();
-        // Draw Board
-        playerCells.setValue(game.getBoard());
-
-        // Set buttons enable/disable
-        Player[][] cells = game.getBoard();
-        isMyTurn = player==Player.FIRST && game.getUserFirstName().equals(username) || player==Player.SECOND && game.getUserSecondName().equals(username);
-
-        for(int row=0;row<cells.length;row++){
-            for(int col=0;col<cells[row].length;col++){
-                Player cell = cells[row][col];
-                _enabledCells[row][col] = game.getGameStatus() == GameStatus.PLAYING && isMyTurn && cell == null;
-            }
-        }
-        enabledCells.setValue(_enabledCells);
-
+    public void processGameUpdate(Game game) {
         GameStatus status = game.getGameStatus();
-
         if (isGameFinished(status)) {
             stopPollingGameUpdates();
-            endGameDialogMessage.setValue(getEndGameMessage(status, game, username));
+            endGameDialogMessage.setValue(getEndGameMessage(status, game));
         }
     }
 
     private boolean isGameFinished(GameStatus status) {
-        return status == GameStatus.PLAYER_1_WIN ||
-                status == GameStatus.PLAYER_2_WIN ||
-                status == GameStatus.PLAYER_1_LEFT ||
-                status == GameStatus.PLAYER_2_LEFT ||
-                status == GameStatus.DRAW;
+        return status == GameStatus.PLAYER_1_WIN || status == GameStatus.PLAYER_2_WIN || status == GameStatus.PLAYER_1_LEFT || status == GameStatus.PLAYER_2_LEFT || status == GameStatus.DRAW;
     }
 
-    private String getEndGameMessage(GameStatus status, Game game, String username) {
+    private String getEndGameMessage(GameStatus status, Game game) {
         Context context = ContextProvider.getApplicationContext();
+        User user = UserRepository.getUser();
+        if (user == null) {
+            return context.getString(R.string.unexpected_status_message, status);
+        }
+        String username = user.getUsername();
         switch (status) {
             case PLAYER_1_WIN:
                 return game.getUserFirstName().equals(username) ? context.getString(R.string.win_message) : context.getString(R.string.lose_message);
@@ -138,8 +113,6 @@ public class TicTacToeViewModel extends ViewModel {
         }
     }
 
-
-
     public void leaveGame(UUID gameId) {
         stopPollingGameUpdates();
         leaveGameSuccess.setValue(true);
@@ -150,19 +123,43 @@ public class TicTacToeViewModel extends ViewModel {
     public void startPollingGameUpdates(UUID gameId) {
         keepUpdating.set(true);
 
-        new Thread(() -> {
-            while(keepUpdating.get()){
-                handleGetGameUpdates(gameId);
-                try {
-                    Thread.sleep(2000);
-                } catch (InterruptedException e) {
-                    return;
-                }
-            }
-        }).start();
+        pollGameUpdates(gameId);
+    }
+
+    private void pollGameUpdates(UUID gameId) {
+        if (!keepUpdating.get()) {
+            return;
+        }
+
+        handleGetGameUpdates(gameId);
+
+        // Schedule the next update after a delay (e.g., 2000 milliseconds)
+        handler.postDelayed(() -> pollGameUpdates(gameId), 2000);
     }
 
     public void stopPollingGameUpdates() {
         keepUpdating.set(false);
+        handler.removeCallbacksAndMessages(null);
+    }
+
+    public void updateBoard(Game game, List<Button> buttons) {
+        User user = UserRepository.getUser();
+        if (user == null) {
+            return;
+        }
+        Player player = game.getCurrentPlayer();
+        String username = user.getUsername();
+        Player[][] cells = game.getBoard();
+        isMyTurn = player == Player.FIRST && game.getUserFirstName().equals(username) || player == Player.SECOND && game.getUserSecondName().equals(username);
+        for (int row = 0; row < cells.length; row++) {
+            for (int col = 0; col < cells[row].length; col++) {
+                int pos = row * cells.length + col;
+                Player cell = cells[row][col];
+                if (cell != null) {
+                    buttons.get(pos).setBackgroundResource(cell == Player.FIRST ? R.drawable.tictactoe_x : R.drawable.tictactoe_o);
+                }
+                buttons.get(pos).setEnabled(game.getGameStatus() == GameStatus.PLAYING && isMyTurn && cell == null);
+            }
+        }
     }
 }
